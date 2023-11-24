923ffbd8cfb8ca490c971b2a6d722b0fb386f160
bruno teleginski <brunotele4@gmail.com>
2023-11-24 09:51:39 -0300

    grupo-15-requisito-1-fix;msg:arruma erros lint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 export async function getCategories() {
-  const urlCategories = "https://api.mercadolibre.com/sites/MLB/categories";
+  const urlCategories = 'https://api.mercadolibre.com/sites/MLB/categories';
   const response = await fetch(urlCategories);
   const dataCategories = response.json();
   return dataCategories;
@@ -7,30 +7,27 @@ export async function getCategories() {
 
 export async function getProductsFromCategoryAndQuery(
   categoryId: string,
-  query: string
+  query: string,
 ) {
-  if(categoryId){
+  if (categoryId) {
     const endPointId = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
     const responseId = await fetch(endPointId);
     const dataCategoriaId = responseId.json();
     return dataCategoriaId;
   }
-  if (query){
+  if (query) {
     const endPointQuery = `https://api.mercadolibre.com/sites/MLB/search?category=${query}`;
     const responseQuery = await fetch(endPointQuery);
     const dataCategoriaQuery = responseQuery.json();
     return dataCategoriaQuery;
   }
-  if(categoryId && query){
+  if (categoryId && query) {
     const endPointIdQuery = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
     const responseIdQuery = await fetch(endPointIdQuery);
     const dataCategoriaIdQuery = responseIdQuery.json();
     return dataCategoriaIdQuery;
   }
 }
-
-
-
 
 export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
