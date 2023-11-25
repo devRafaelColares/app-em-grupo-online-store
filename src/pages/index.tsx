c23a69d8c417b654008863632b454c88c914e003
Rafael Colares <rafaelcolares.dev@gmail.com>
2023-11-25 11:35:58 -0300

    Grupo-15-requisito-6-feat;msg: Filtra produtos por categoria./ adicionei comentarios para legibilidade do codigo.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 import CategoryList from '../components/CategoryList';
 import ProductList from '../components/ProductList';
+import { Category } from '../types';
 
-interface Category {
-  id: string;
-  name: string;
-}
-
+// Componente principal da página inicial
 function Home() {
+  // Estado para armazenar a lista de categorias
   const [categoriesList, setCategoriesList] = useState<Category[]>([]);
+  // Estado para armazenar a categoria selecionada
   const [selectedCategory, setSelectedCategory] = useState<string>('');
+  // Estado para armazenar a string de consulta de pesquisa
   const [searchQuery, setSearchQuery] = useState<string>('');
-  const [productList, setProductList] = useState<[]>([]);
+  // Estado para armazenar a lista de produtos
+  const [productList, setProductList] = useState<any[]>([]);
 
+  // Efeito para carregar as categorias ao montar o componente
   useEffect(() => {
     const fetchCategories = async () => {
       const dataCategories = await getCategories();
@@ -26,14 +28,29 @@ function Home() {
     fetchCategories();
   }, []);
 
-  const handleCategoryChange = (categoryId: string) => {
+  // Função para lidar com a mudança de categoria
+  const handleCategoryChange = async (categoryId: string) => {
     setSelectedCategory(categoryId);
+    await getProductsByCategory(categoryId);
   };
 
+  // Função para obter os produtos de uma categoria específica
+  const getProductsByCategory = async (categoryId: string) => {
+    try {
+      const searchData = await getProductsFromCategoryAndQuery(categoryId, '');
+      setProductList(searchData.results);
+    } catch (error) {
+      console.error('Erro ao buscar produtos por categoria', error);
+      setProductList([]);
+    }
+  };
+
+  // Função para lidar com a mudança na entrada de pesquisa
   const handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value);
   };
 
+  // Função para lidar com a pesquisa de produtos
   const handleSearch = async () => {
     try {
       const searchData = await
@@ -50,6 +67,7 @@ function Home() {
     }
   };
 
+  // Estrutura JSX que renderiza a página inicial
   return (
     <div>
       <input
@@ -58,22 +76,26 @@ function Home() {
         value={ searchQuery }
         data-testid="query-input"
       />
-      <button onClick={ handleSearch } data-testid="query-button">Pesquisar</button>
+      <button onClick={ handleSearch } data-testid="query-button">
+        Pesquisar
+      </button>
 
       <h2>Categorias</h2>
+      {/* Componente de lista de categorias */}
       <CategoryList
         categories={ categoriesList }
         selectedCategory={ selectedCategory }
         onCategoryChange={ handleCategoryChange }
       />
+      {/* Condicional para renderizar mensagem ou lista de produtos */}
       {productList.length === 0 ? (
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
       ) : (
         <ProductList productList={ productList } />
-
       )}
+      {/* Link para o carrinho de compras */}
       <Link to="/carrinho" data-testid="shopping-cart-button">
         Carrinho de Compras
       </Link>
